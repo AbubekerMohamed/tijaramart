4e104720e6e6268d142593633911787ad8a5cbc3
AbubekerMohammed <abumoha1892@gmail.com>
2024-01-06 10:22:39 +0300

    search app bar added

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tijaramart/constants/global_variables.dart';
 import 'package:tijaramart/providers/user_provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,10 +17,82 @@ class _HomeScreenState extends State<HomeScreen> {
     // get the user data
     final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
-      body: Center(
-        child: Text(
-          user.toJson(),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 42,
+                  margin: const EdgeInsets.only(left: 15),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(7),
+                    elevation: 1,
+                    child: TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: InkWell(
+                              onTap: () {},
+                              child: const Padding(
+                                padding: EdgeInsets.only(left: 6),
+                                child: Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                  size: 23,
+                                ),
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: const EdgeInsets.only(
+                              top: 10,
+                            ),
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(7),
+                              ),
+                              borderSide: BorderSide.none,
+                            ),
+                            enabledBorder: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(7),
+                              ),
+                              borderSide: BorderSide(
+                                color: Colors.black38,
+                                width: 1,
+                              ),
+                            ),
+                            hintText: "Search tijaramart...",
+                            hintStyle: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17,
+                            ))),
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.transparent,
+                height: 42,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: const Icon(
+                  Icons.mic,
+                  size: 25,
+                ),
+              )
+            ],
+          ),
         ),
+      ),
+      body: const Column(
+        children: [],
       ),
     );
   }
