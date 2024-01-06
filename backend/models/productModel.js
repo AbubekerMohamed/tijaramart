b5aeb39a6f2c92f0bf2a86a03970ef4f202ea989
Abubeker Mohammed <abumoha1892@gmail.com>
2024-01-06 22:46:55 +0300

    added product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  description: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  images: [{ type: String, required: true }],
+  quantity: {
+    required: true,
+    type: Number,
+  },
+  price: {
+    required: true,
+    type: Number,
+  },
+  category: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+});
+
+const ProductModel = mongoose.model("Product", productSchema);
+module.exports = ProductModel;
