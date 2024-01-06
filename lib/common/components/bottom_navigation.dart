1bc47c55e0e16d5df81dbede81ad21b9b5034c30
AbubekerMohammed <abumoha1892@gmail.com>
2024-01-06 13:32:25 +0300

    GNav curve added

--- lib/common/components/bottom_navigation.dart
+++ lib/common/components/bottom_navigation.dart
@@ -30,46 +30,52 @@ class _BottomNavigationState extends State<BottomNavigation> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: pages[_page]),
-      bottomNavigationBar: Container(
-        color: Colors.purple[200],
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 15.0,
-            vertical: 10.0,
-          ),
-          child: GNav(
-            iconSize: 28,
-            backgroundColor: Colors.purple[200]!,
-            color: Colors.black87,
-            activeColor: Colors.white,
-            tabBackgroundColor: Colors.deepPurple,
-            padding: const EdgeInsets.all(16),
-            gap: 5,
-            onTabChange: (index) {
-              updatePageNumber(index);
-            },
-            tabs: [
-              const GButton(
-                icon: Icons.home,
-                text: "Home",
-              ),
-              GButton(
-                icon: Icons.shopping_cart,
-                text: "Cart",
-                leading: Badge(
-                  label: const Text("1"),
-                  offset: const Offset(12, -12),
-                  child: Icon(
-                    Icons.shopping_cart,
-                    color: _page == 1 ? Colors.white : Colors.black,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: Container(
+          color: Colors.purple[200],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical: 10.0,
+            ),
+            child: GNav(
+              iconSize: 28,
+              backgroundColor: Colors.purple[200]!,
+              color: Colors.black87,
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.deepPurple,
+              padding: const EdgeInsets.all(16),
+              gap: 5,
+              onTabChange: (index) {
+                updatePageNumber(index);
+              },
+              tabs: [
+                const GButton(
+                  icon: Icons.home,
+                  text: "Home",
+                ),
+                GButton(
+                  icon: Icons.shopping_cart,
+                  text: "Cart",
+                  leading: Badge(
+                    label: const Text("1"),
+                    offset: const Offset(12, -12),
+                    child: Icon(
+                      Icons.shopping_cart,
+                      color: _page == 1 ? Colors.white : Colors.black,
+                    ),
                   ),
                 ),
-              ),
-              const GButton(
-                icon: Icons.person,
-                text: "Profile",
-              ),
-            ],
+                const GButton(
+                  icon: Icons.person,
+                  text: "Profile",
+                ),
+              ],
+            ),
           ),
         ),
       ),
