671425f8015654be9e26c2452f2dd03f2cdf2b0e
AbubekerMohammed <abumoha1892@gmail.com>
2024-01-10 09:58:02 +0300

    rating method added

---
+++ lib/features/product_details/services/product_details_service.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:tijaramart/common/components/snackbar.dart';
+import 'package:tijaramart/constants/error_handlers.dart';
+import 'package:tijaramart/constants/global_variables.dart';
+import 'package:tijaramart/models/product_nodel.dart';
+import 'package:tijaramart/providers/user_provider.dart';
+
+class ProductDetailsService {
+  void rateProduct({
+    required BuildContext context,
+    required ProductModel product,
+    required double rating,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    try {
+      http.Response response = await http.post(
+        Uri.parse('$backendURL/api/rate-product'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        },
+        body: jsonEncode(
+          {
+            'id': product.id!,
+            'rating': rating,
+          },
+        ),
+      );
+
+      if (context.mounted) {
+        httpErrorHandle(
+          response: response,
+          context: context,
+          onSuccess: () {},
+        );
+      }
+    } catch (error) {
+      if (context.mounted) {
+        showSnackBar(context, "Product Details Error$error");
+      }
+    }
+  }
+}
