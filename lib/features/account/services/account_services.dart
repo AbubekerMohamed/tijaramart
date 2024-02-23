71f7617f5b611f0af86bbb5ba22a2cf08cb3786c
Abubeker Mohammed <abumoha1892@gmail.com>
2024-02-23 10:46:32 +0300

    added fetch orders client call

---
+++ lib/features/account/services/account_services.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:tijaramart/common/components/snackbar.dart';
+import 'package:tijaramart/constants/error_handlers.dart';
+import 'package:tijaramart/constants/global_variables.dart';
+import 'package:tijaramart/models/product_nodel.dart';
+import 'package:tijaramart/providers/user_provider.dart';
+
+class AddressServices {
+  Future<List<ProductModel>> fetchMyOrders({
+    required BuildContext context,
+  }) async {
+    List<ProductModel> ordersList = [];
+    try {
+      http.Response response = await http.get(
+        Uri.parse("$backendURL/api/orders/me"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': context.mounted
+              ? Provider.of<UserProvider>(
+                  context,
+                  listen: false,
+                ).user.token
+              : '',
+        },
+      );
+      if (context.mounted) {
+        httpErrorHandle(
+          response: response,
+          context: context,
+          onSuccess: () {
+            for (var i = 0; i < jsonDecode(response.body); i++) {
+              ordersList.add(
+                ProductModel.fromJson(
+                  jsonEncode(
+                    jsonDecode(response.body[i]),
+                  ),
+                ),
+              );
+            }
+          },
+        );
+      }
+    } catch (error) {
+      if (context.mounted) showSnackBar(context, error.toString());
+    }
+    return ordersList;
+  }
+}
