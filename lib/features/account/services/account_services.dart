05019d7e6cdbb2568fbba69dbc2817282af64252
Abubeker Mohammed <abumoha1892@gmail.com>
2024-02-23 11:11:12 +0300

    added: order model

--- lib/features/account/services/account_services.dart
+++ lib/features/account/services/account_services.dart
@@ -6,14 +6,14 @@ import 'package:provider/provider.dart';
 import 'package:tijaramart/common/components/snackbar.dart';
 import 'package:tijaramart/constants/error_handlers.dart';
 import 'package:tijaramart/constants/global_variables.dart';
-import 'package:tijaramart/models/product_nodel.dart';
+import 'package:tijaramart/models/order_model.dart';
 import 'package:tijaramart/providers/user_provider.dart';
 
 class AddressServices {
-  Future<List<ProductModel>> fetchMyOrders({
+  Future<List<OrderModel>> fetchMyOrders({
     required BuildContext context,
   }) async {
-    List<ProductModel> ordersList = [];
+    List<OrderModel> ordersList = [];
     try {
       http.Response response = await http.get(
         Uri.parse("$backendURL/api/orders/me"),
@@ -34,7 +34,7 @@ class AddressServices {
           onSuccess: () {
             for (var i = 0; i < jsonDecode(response.body); i++) {
               ordersList.add(
-                ProductModel.fromJson(
+                OrderModel.fromJson(
                   jsonEncode(
                     jsonDecode(response.body[i]),
                   ),
---
+++ lib/models/order_model.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:tijaramart/models/product_nodel.dart';
+
+class OrderModel {
+  final String id;
+  final List<ProductModel> products;
+  final List<int> quantity;
+  final String address;
+  final String userId;
+  final int orderedAt;
+  final int status;
+
+  OrderModel({
+    required this.id,
+    required this.products,
+    required this.quantity,
+    required this.address,
+    required this.userId,
+    required this.orderedAt,
+    required this.status,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'products': products.map((x) => x.toMap()).toList(),
+      'quantity': quantity,
+      'address': address,
+      'userId': userId,
+      'orderedAt': orderedAt,
+      'status': status,
+    };
+  }
+
+  factory OrderModel.fromMap(Map<String, dynamic> map) {
+    return OrderModel(
+      id: map['id'] ?? '',
+      products: List<ProductModel>.from(
+        map['products']?.map(
+          (x) => ProductModel.fromMap(x['product']),
+        ),
+      ),
+      quantity: List<int>.from((map['products']?.map((x) => x['quantity']))),
+      address: map['address'] ?? '',
+      userId: map['userId'] ?? '',
+      orderedAt: map['orderedAt']?.toInt() ?? 0,
+      status: map['status']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory OrderModel.fromJson(String source) =>
+      OrderModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
