954033c6453d82df1318338b20eec9c25d9ca0b1
Abubeker Mohammed <abumoha1892@gmail.com>
2024-02-24 10:49:30 +0300

    added: api to get total earning analytics

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -60,7 +60,7 @@ adminRouter.get("/admin/get-orders", adminMiddleware, async (req, res) => {
   }
 });
 
-// delete product route
+// update order status route
 adminRouter.post(
   "/admin/update-order-status",
   adminMiddleware,
@@ -80,5 +80,54 @@ adminRouter.post(
     }
   }
 );
+
+// get total earnings analysis
+adminRouter.get("/admin/analytics", adminMiddleware, async (req, res) => {
+  try {
+    const orders = await OrderModel.find({});
+    let totalEarnings = 0;
+
+    for (let i = 0; i < orders.length; i++) {
+      for (let j = 0; j < orders[i].products.length; j++) {
+        totalEarnings +=
+          orders[i].products[j].quantity * orders[i].products[j].product.price;
+      }
+    }
+    // fetch by category
+    const mobileEarnings = await fetchTotalEarningsByCategory("Mobiles");
+    const essentialEarnings = await fetchTotalEarningsByCategory("Essentials");
+    const applianceEarnings = await fetchTotalEarningsByCategory("Appliances");
+    const bookEarnings = await fetchTotalEarningsByCategory("Books");
+    const fashionEarnings = await fetchTotalEarningsByCategory("Fashions");
+
+    const earnings = {
+      totalEarnings,
+      mobileEarnings,
+      essentialEarnings,
+      applianceEarnings,
+      bookEarnings,
+      fashionEarnings,
+    };
+
+    res.json(earnings);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+async function fetchTotalEarningsByCategory(category) {
+  let earnings = 0;
+  let ordersInCategory = OrderModel.find({
+    "products.product.category": category,
+  });
+  for (let i = 0; i < ordersInCategory.length; i++) {
+    for (let j = 0; j < ordersInCategory[i].products.length; j++) {
+      earnings +=
+        ordersInCategory[i].products[j].quantity *
+        ordersInCategory[i].products[j].product.price;
+    }
+  }
+  return earnings;
+}
 
 module.exports = adminRouter;
